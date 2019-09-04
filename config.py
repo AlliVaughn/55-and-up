bd969087f4d655cb520095c2d32e6bff3dd9d439
Alli Vaughn <alli@polycotassociates.com>
2019-09-04 10:24:02 -0500

    cleaned up config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 # This is the config file for our project.  Only one set of credentials will be saved here for the final project. 
 
 # Alli's credentials 
 # api_key: ""
 # gkey:""
     
-=======
+
 # add config here 
->>>>>>> 179c1a15cb9633809a518024235fd68befdec132
+
